2098e1b5823f1d23d73bf3a2ec7439af0326fccb
BhawishyaAccio <bhawishya@acciojob.com>
2025-05-10 04:53:23 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,5 +1,15 @@
 function fibonacci(num) {
-// your code here
+if(num==1) return 0;
+else if(num==1)return 1
+	let a=0,b=1,c=0'
+for(let i=3;i<num;i++)
+	{
+		c=a+b;
+		a=b;
+		b=c;
+	}
+	return b;
+	
 }
 
 module.exports = fibonacci;
